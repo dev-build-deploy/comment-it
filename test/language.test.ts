f29a65246b7cf46f125b223297e537b6376adf73
Kevin de Jong <monkaii@hotmail.com>
2023-11-17 11:04:08 +0100

    chore(test): validate language.JSON file as part of tests

--- test/language.test.ts
+++ test/language.test.ts
@@ -3,7 +3,9 @@ SPDX-FileCopyrightText: 2023 Kevin de Jong <monkaii@hotmail.com>
 SPDX-License-Identifier: MIT
 */
 
+import { ILanguage } from "../src";
 import { isSupported, addLanguage, getLanguageToken } from "../src/languages";
+import { languages } from "../src/languages/languages.json";
 
 describe("Languages", () => {
   const testData = [
@@ -41,6 +43,34 @@ describe("Languages", () => {
 
   it.each(testData)("$description", ({ file, isSupported: expectations }) => {
     expect(isSupported(file as string)).toBe(expectations);
+  });
+});
+
+describe("Check for duplication in languages", () => {
+  test("Check for filename duplications", () => {
+    const filenames = new Set<string>();
+    const duplicates = new Set<string>();
+    for (const language of languages) {
+      for (const filename of language.filenames ?? []) {
+        if (filenames.has(filename)) duplicates.add(filename);
+        else filenames.add(filename);
+      }
+    }
+
+    expect(duplicates).toEqual(new Set<string>());
+  });
+
+  test("Check for extension duplications", () => {
+    const extensions = new Set<string>();
+    const duplicates = new Set<string>();
+    for (const language of languages) {
+      for (const extension of language.extensions ?? []) {
+        if (extensions.has(extension)) duplicates.add(extension);
+        else extensions.add(extension);
+      }
+    }
+
+    expect(duplicates).toEqual(new Set<string>());
   });
 });
 
