b2bf3064411cf5d71ebf1393556a7e1b75910bb4
Kevin de Jong <monkaii@hotmail.com>
2023-11-24 11:30:44 +0100

    fix: export ICommentContent without renaming

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ SPDX-License-Identifier: MIT
 
 export { addLanguage, getLanguage, isSupported } from "./languages";
 export { extractComments } from "./extractor";
-export { IComment, ICommentContent as ICommentContentType, IExtractorOptions, ILanguage } from "./interfaces";
+export { IComment, ICommentContent, IExtractorOptions, ILanguage } from "./interfaces";
