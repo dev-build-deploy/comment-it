8aab6aac9375e26b4f41cc9be73c3b25fddf8483
Xavier Martin <Xavier.martin@tomtom.com>
2024-05-27 09:28:17 +0200

    fix: improve overall time execution
    Optimize the time execution focusing on two different aspects
    regarding the way we match languages with a file:
    
     - Remove the use of the 'globSync' function. This function is
    called each time a wildcard pattern is found when adding a new
    language. The function analyzes every time all the files inside
    the directory, and gets really slow when the number of files
    increase substantially. In the second bullet point it is explained
    how it can be fully removed.
    
     - There is redundant work being done twice: matching languages
    with a single file. First, it is done in the 'globSync' when
    adding a new language (matched against all files). Secondly, it
    is done in the 'getLanguageMatches'. Therefore, as all public
    methods call the 'getLanguageMatches' function, we just need to
    check in it which languages match with the current file, without
    the need to precompute it previously in the 'replaceGlobPatternsInFilenames'
    function.

--- src/languages.ts
+++ src/languages.ts
@@ -4,40 +4,12 @@ SPDX-License-Identifier: MIT
 */
 
 import path from "path";
-
-import { sync as globSync } from "glob";
+import micromatch from 'micromatch';
 
 import { languages } from "./languages/languages.json";
 import { Language, LanguageTokens } from "./types";
 
-const LANGUAGES = replaceGlobPatternsInLanguages(languages as Language[]);
-
-/**
- * Replaces any glob patterns in the filenames with actual filenames
- * @param filenames The filenames to replace the glob patterns in
- * @returns The filenames with the glob patterns replaced with relative path filenames
- */
-function replaceGlobPatternsInFilenames(filenames?: string[]): string[] | undefined {
-  if (filenames === undefined) return undefined;
-  return filenames.flatMap(filename => {
-    if (filename.includes("*")) return globSync(filename, { dot: true });
-    else return filename;
-  });
-}
-
-/**
- * Converts any glob patterns in the filenames to actual filenames
- * @param languages The languages to replace the glob patterns in
- * @returns The languages with the glob patterns replaced with relative path filenames
- */
-function replaceGlobPatternsInLanguages(languages: Language[]): Language[] {
-  const results = JSON.parse(JSON.stringify(languages)) as Language[];
-  results.forEach(language => {
-    language.filenames = replaceGlobPatternsInFilenames(language.filenames);
-  });
-
-  return results;
-}
+const LANGUAGES = languages as Language[];
 
 /**
  * Returns the languages that match the provided file
@@ -48,7 +20,8 @@ function replaceGlobPatternsInLanguages(languages: Language[]): Language[] {
 function getLanguageMatches(file: string): [Language[], Language[]] {
   const extension = path.extname(file);
   const filenameMatches = LANGUAGES.filter(language =>
-    language.filenames?.map(filename => path.resolve(filename)).includes(path.resolve(file))
+    language.filenames != undefined &&
+    micromatch.isMatch(file, language.filenames, { dot: true })
   );
   const extensionMatches = LANGUAGES.filter(language => language.extensions?.includes(extension));
 
@@ -93,13 +66,11 @@ export function getLanguage(file: string): Language {
  * Extends the list of supported languages with the provided language.
  * NOTES:
  *   - Adding any new language including overlapping filenames or extensions,
- *   - Glob patterns will be applied upon adding the language
  * will override any previously existing language (incl. defaults).
  *
  * @param language The language to add
  */
 export function addLanguage(language: Language): void {
-  language.filenames = replaceGlobPatternsInFilenames(language.filenames);
   LANGUAGES.unshift(language);
 }
 
